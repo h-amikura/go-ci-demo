fb7ede989988364f71a458d1c5af1fc94fb905c8
Hideaki Amikura <hideaki05233@gmail.com>
2025-05-02 19:11:03 +0900

    fix: ensure server stays alive on port 80

--- main.go
+++ main.go
@@ -1,32 +1,16 @@
 package main
 
 import (
-    "database/sql"
-    "fmt"
-    "log"
-    "net/http"
-    _ "github.com/lib/pq"
+	"fmt"
+	"log"
+	"net/http"
 )
 
 func main() {
-    dbURL := "postgres://postgres:mysecretpassword@host.docker.internal:5432/postgres?sslmode=disable"
-    db, err := sql.Open("postgres", dbURL)
-    if err != nil {
-        log.Println("DB接続エラー:", err)
-    } else {
-        defer db.Close()
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Goアプリがポート80で起動中です！")
+	})
 
-        if err = db.Ping(); err != nil {
-            log.Println("DB応答なし:", err)
-        } else {
-            fmt.Println("DB接続成功！")
-        }
-    }
-
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Goアプリがポート80で起動中です！")
-    })
-
-    log.Println(">>> ポート80でサーバーを起動します...")
-    log.Fatal(http.ListenAndServe(":80", nil))
+	log.Println(">>> ポート80でサーバーを起動します...")
+	log.Fatal(http.ListenAndServe(":80", nil))
 }
