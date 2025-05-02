a7088cc34543f26f07714938f9e637e2af61887f
Hideaki Amikura <hideaki05233@gmail.com>
2025-05-02 18:57:49 +0900

    fix: run http server on port 80

--- main.go
+++ main.go
@@ -1,12 +1,34 @@
+
 package main
 
-import "fmt"
-
-// Hello returns a greeting message.
-func Hello() string {
-	return "Hello, CI!"
-}
+import (
+    "database/sql"
+    "fmt"
+    "log"
+    "net/http"
+    _ "github.com/lib/pq"
+)
 
 func main() {
-	fmt.Println(Hello())
+    dbURL := "postgres://postgres:mysecretpassword@host.docker.internal:5432/postgres?sslmode=disable"
+    db, err := sql.Open("postgres", dbURL)
+    if err != nil {
+        log.Fatal("DB接続エラー:", err)
+    }
+    defer db.Close()
+
+    err = db.Ping()
+    if err != nil {
+        log.Fatal("DB応答なし:", err)
+    }
+
+    fmt.Println("DB接続成功！")
+
+    // ポート80でHTTPサーバー起動
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        fmt.Fprintf(w, "DB接続成功！Goサーバーは正常に動いています。")
+    })
+
+    log.Println("サーバーをポート80で起動中...")
+    log.Fatal(http.ListenAndServe(":80", nil))
 }
