d9ed601f47efa840ef66dfb31d67af15c0f31953
Hideaki Amikura <hideaki05233@gmail.com>
2025-05-02 19:08:11 +0900

    fix: Go HTTP server added for Azure

--- main.go
+++ main.go
@@ -1,4 +1,3 @@
-
 package main
 
 import (
@@ -13,22 +12,21 @@ func main() {
     dbURL := "postgres://postgres:mysecretpassword@host.docker.internal:5432/postgres?sslmode=disable"
     db, err := sql.Open("postgres", dbURL)
     if err != nil {
-        log.Fatal("DB接続エラー:", err)
+        log.Println("DB接続エラー:", err)
+    } else {
+        defer db.Close()
+
+        if err = db.Ping(); err != nil {
+            log.Println("DB応答なし:", err)
+        } else {
+            fmt.Println("DB接続成功！")
+        }
     }
-    defer db.Close()
 
-    err = db.Ping()
-    if err != nil {
-        log.Fatal("DB応答なし:", err)
-    }
-
-    fmt.Println("DB接続成功！")
-
-    // ポート80でHTTPサーバー起動
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "DB接続成功！Goサーバーは正常に動いています。")
+        fmt.Fprintf(w, "Goアプリがポート80で起動中です！")
     })
 
-    log.Println("サーバーをポート80で起動中...")
+    log.Println(">>> ポート80でサーバーを起動します...")
     log.Fatal(http.ListenAndServe(":80", nil))
 }
