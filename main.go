df43dcb8df64d9494d912689e224ff627b403a50
Hideaki Amikura <hideaki05233@gmail.com>
2025-05-02 19:23:28 +0900

    fix: final version with port 80 server

--- main.go
+++ main.go
@@ -4,13 +4,20 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
+	// Azure App Service から渡されるポート番号を取得（なければ80）
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "80"
+	}
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Goアプリがポート80で起動中です！")
+		fmt.Fprintf(w, "Goアプリがポート%sで起動中です！", port)
 	})
 
-	log.Println(">>> ポート80でサーバーを起動します...")
-	log.Fatal(http.ListenAndServe(":80", nil))
+	log.Printf(">>> ポート%sでサーバーを起動します...\n", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
